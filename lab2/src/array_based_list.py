12a202f762f3fe213ab2154c3a8fcbb333a81664
BANERR <banerr2006@gmail.com>
2025-03-28 20:53:06 +0200

    added deleteAll, get, clone, delete functions for lab2

--- lab2/src/array_based_list.py
+++ lab2/src/array_based_list.py
@@ -13,6 +13,24 @@ class ArrayBasedList:
             raise IndexError("Index out of range")
         self.data.insert(index, element)
 
+    def delete(self, index):
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of range")
+        return self.data.pop(index)
+
+    def deleteAll(self, element):
+        self.data = [x for x in self.data if x != element]
+
+    def get(self, index):
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of range")
+        return self.data[index]
+
+    def clone(self):
+        cloned_list = ArrayBasedList()
+        cloned_list.data = self.data.copy()
+        return cloned_list
+
 
     def __str__(self):
         return str(self.data)
@@ -20,7 +38,7 @@ class ArrayBasedList:
 if __name__ == "__main__":
     my_list = ArrayBasedList()
     while True:
-        command = input("Enter command (append, insert): ").strip()
+        command = input("Enter command (append, insert, delete, get, deleteAll, clone): ").strip()
         if command == "append":
             value = input("Enter character: ")
             my_list.append(value)
@@ -29,6 +47,23 @@ if __name__ == "__main__":
             index = int(input("Enter index: "))
             try:
                 my_list.insert(value, index)
+            except IndexError as e:
+                print(e)
+        elif command == "delete":
+            index = int(input("Enter index: "))
+            try:
+                print("Deleted:", my_list.delete(index))
+            except IndexError as e:
+                print(e)
+        elif command == "deleteAll":
+            value = input("Enter character: ")
+            my_list.deleteAll(value)
+        elif command == "clone":
+            my_list.clone()
+        elif command == "get":
+            index = int(input("Enter index: "))
+            try:
+                print("Value:", my_list.get(index))
             except IndexError as e:
                 print(e)
         else:
